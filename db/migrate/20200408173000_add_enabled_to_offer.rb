f4e268ee14f328c47fe8b2ab470b832797d84fb9
Vinicius 'VinyLimaZ' Lima <vinylimaz@live.com>
2020-04-09 17:17:14 -0300

    Sets the state to be enabled on offer creation

--- db/migrate/20200408173000_add_enabled_to_offer.rb
+++ db/migrate/20200408173000_add_enabled_to_offer.rb
@@ -1,5 +1,5 @@
 class AddEnabledToOffer < ActiveRecord::Migration[6.0]
   def change
-    add_column :offers, :enabled, :boolean, null: true
+    add_column :offers, :enabled, :boolean, default: true
   end
 end
